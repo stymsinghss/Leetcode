c0c0fc4e77e25a32ebc5681f7dfb6267391493ed
stymsinghss <iamstymsingh@gmail.com>
2025-03-16 22:29:51 +0530

    11. Container With Most Water

---
+++ Leetcode 75/11. Container With Most Water.py
@@ -0,0 +1,14 @@
+from typing import  List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_water = 0
+
+        left, right = 0, len(height)-1
+        while left <= right:
+            max_water = max(max_water, min(height[left], height[right])*(right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_water
